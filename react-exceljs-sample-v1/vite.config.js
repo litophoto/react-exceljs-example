230ef085f744fad8df73fb1f6ce45ef71e05ee21
litophoto <ciro17581@gmail.com>
2022-12-10 23:28:32 +0900

    Add ライブラリの追加(polyfill, buffer, file-server, stream-browserify, xlsx-populate)主にエクセルに接続し、出力をするためのツールを追加 また、これらのライブラリーをvite-react上で扱うための設定を追記した

--- react-exceljs-sample-v1/vite.config.js
+++ react-exceljs-sample-v1/vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // build: {
+  //   commonjsOptions: {
+  //     ignoreTryCatch: id => id !== 'stream',
+  //   },
+  // },
+  resolve: {
+    alias: [
+      {
+        find: 'stream',
+        replacement: `stream-browserify`,
+      },
+    ],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          // process: true,
+          buffer: true
+        })
+      ]
+    }
+  }
 })
