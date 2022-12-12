58204c5b5167e46c9e211fd5086f4837e8e7b4de
litophoto <ciro17581@gmail.com>
2022-12-12 10:40:42 +0900

    Add データセットの追加

---
+++ react-exceljs-sample-v1/src/xlsx-populate/excel-data/starter-pokemon.v1.js
@@ -0,0 +1,50 @@
+const starterPokemons = {
+    "FirstGeneration": {
+        "title": "第一世代 御三家",
+        "GrassName": "フシギダネ",
+        "GrassType": "くさ どく",
+        "GrassAvility": "しんりょく",
+        "GrassFinally": "フシギバナ",
+        "WaterName": "ゼニガメ",
+        "WaterType": "みず",
+        "WaterAvility": "げきりゅう",
+        "WaterFinally": "カメックス",
+        "FireName": "ヒトカゲ",
+        "FireType": "ほのお",
+        "FireAvility": "もうか",
+        "FireFinally": "リザードン"
+    },
+    "SecondGeneration": {
+        "title": "第二世代 御三家",
+        "GrassName": "チコリータ",
+        "GrassType": "くさ",
+        "GrassAvility": "しんりょく",
+        "GrassFinally": "メガニウム",
+        "WaterName": "ワニノコ",
+        "WaterType": "みず",
+        "WaterAvility": "げきりゅう",
+        "WaterFinally": "アリゲーツ",
+        "FireName": "ヒノアラシ",
+        "FireType": "ほのお",
+        "FireAvility": "もうか",
+        "FireFinally": "バクフーン"
+    },
+    "NinthGeneration": {
+        "title": "第九世代 御三家",
+        "GrassName": "ニャオハ",
+        "GrassType": "くさ",
+        "GrassAvility": "しんりょく",
+        "GrassFinally": "マスカーニャ",
+        "WaterName": "クワッス",
+        "WaterType": "みず",
+        "WaterAvility": "げきりゅう",
+        "WaterFinally": "ウェーニバル",
+        "FireName": "ホゲータ",
+        "FireType": "ほのお",
+        "FireAvility": "もうか",
+        "FireFinally": "ラウドボーン"
+    },
+
+}
+
+export { starterPokemons }
