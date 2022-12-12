3b1efa2471b60ab61c36cb5258a9ba3a021053ff
litophoto <ciro17581@gmail.com>
2022-12-12 10:46:21 +0900

    Change 簡単なサンプルを作成 機能を使っているだけで、具体的な実装はその先の関数に集約

--- react-exceljs-sample-v1/src/xlsx-populate/sample-v2.jsx
+++ react-exceljs-sample-v1/src/xlsx-populate/sample-v2.jsx
@@ -1,14 +1,17 @@
-import { getWorkbook, downloadWorkbook } from "./functions/xlsxPopulateManager"
+import { getWorkbook, downloadWorkbook, setDataToWorkbook } from "./functions/xlsxPopulateManager";
+import { starterPokemons } from "./excel-data/starter-pokemon.v1";
+const starterPokemon1stGeneration = starterPokemons["FirstGeneration"];
 
-
-const url = '/sv-starter-pokemons.v1.xlsx'
+const url = "/sv-starter-pokemons.v1.xlsx";
 const ExcelExportButton = () => {
-    const onExport = async () => {
-        const workbook = await getWorkbook(url)
-        await downloadWorkbook(workbook, 'output.xlsx')
-    }
+  const onExport = async () => {
+    const workbook = await getWorkbook(url);
+    
+    const firstPokemonWorkbook = await setDataToWorkbook(workbook, starterPokemon1stGeneration)
+    await downloadWorkbook(firstPokemonWorkbook, "output.xlsx");
+  };
 
-    return <button onClick={onExport} >Export</button>
-}
+  return <button onClick={onExport}>Export</button>;
+};
 
-export default ExcelExportButton
+export default ExcelExportButton;
